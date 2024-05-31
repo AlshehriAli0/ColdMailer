16ab801541dc6e9dd51d25aed7dd3750af710abf
Ali <134097349+AlshehriAli0@users.noreply.github.com>
2024-05-31 13:58:07 +0300

    Update suspense.tsx

--- src/components/suspense/suspense.tsx
+++ src/components/suspense/suspense.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Skeleton() {
   return (
-    <div className="h-12 w-12 animate-pulse rounded-full bg-gray-300"></div>
+    <div className="h-12 w-12 animate-pulse rounded-full bg-gray-300 "></div>
     
   );
 }
