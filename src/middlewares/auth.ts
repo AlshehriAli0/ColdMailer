50e18d7195a0d711e1f9d13098888651d60755e5
Ali <134097349+AlshehriAli0@users.noreply.github.com>
2024-06-02 23:14:26 +0300

    Update auth.ts

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,6 +3,6 @@ import type { MiddlewareWrapperType } from "./types";
 
 export const auth: MiddlewareWrapperType = () => {
   return authMiddleware({
-    publicRoutes: ["/", "/sign-in", "/sign-up", "/coldMailerIcon.png"],
+    publicRoutes: ["/", "/sign-in", "/sign-up", "/coldMailerIcon"],
   });
 };
