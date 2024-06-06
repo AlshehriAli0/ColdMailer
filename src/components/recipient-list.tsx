91e71b41f1ff69e93e5dfef0cebfa4455fc72c10
Ali Alshehri <Ali0fawzish@outlook.com>
2024-06-06 13:38:24 +0300

    recipient component refactoring

--- src/components/recipient-list.tsx
+++ src/components/recipient-list.tsx
@@ -13,9 +13,8 @@ import {
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { sortRecipients } from "@/utils/helpers";
 import { motion, AnimatePresence } from "framer-motion";
-import { BsThreeDots } from "react-icons/bs";
 import clsx from "clsx";
-import { GoDotFill } from "react-icons/go";
+import RecipientRow from "@/components/recipient-row";
 
 const EditRecipient = React.lazy(() => import("@/components/edit-recipient"));
 
@@ -104,40 +103,10 @@ export default function RecipientList({
                 className="grid h-full grid-cols-5 gap-4 text-center"
                 style={{ gridTemplateColumns: "4fr 4fr 1fr 1fr 0.3fr" }}
               >
-                <p className="no-scrollbar mx-auto flex w-[5rem] items-center overflow-x-auto whitespace-nowrap text-xs sm:text-sm md:mx-0 md:w-full md:min-w-fit md:text-base">
-                  {recipient.email_address}
-                </p>
-                <p className="no-scrollbar mx-auto flex w-16 items-center overflow-x-auto whitespace-nowrap text-xs sm:text-sm md:mx-0 md:ml-2 md:w-full md:text-base">
-                  {recipient.name}
-                </p>
-                <p className="mx-auto flex items-center text-xs sm:text-sm md:mx-0 md:text-base">
-                  <span
-                    className={clsx(
-                      recipient.status === "accepted"
-                        ? "text-green-600/90"
-                        : "",
-                      recipient.status === "pending" ? "text-gray-600/90" : "",
-                      recipient.status === "rejected" ? "text-red-600/90" : "",
-                      "mr-1 items-center justify-center text-xs",
-                    )}
-                  >
-                    <GoDotFill />
-                  </span>
-                  {recipient.status}
-                </p>
-                <p className="mx-auto flex items-center text-xs sm:text-sm md:mx-0 md:text-base">
-                  {recipient.sent_at
-                    ? new Date(recipient.sent_at).toLocaleDateString()
-                    : ""}
-                </p>
-                <span className="mx-auto flex items-center text-xs sm:text-sm md:mx-0 md:text-base">
-                  <button
-                    onClick={() => handleEditClick(recipient)}
-                    className="-ml-2 flex h-8 w-8 items-center justify-center rounded text-violet-400 transition-all hover:scale-[1.17] hover:bg-white/5 hover:text-violet-200 active:scale-100 md:-ml-0 md:w-10"
-                  >
-                    <BsThreeDots />
-                  </button>
-                </span>
+                <RecipientRow
+                  recipient={recipient}
+                  handleEditClick={handleEditClick}
+                />
               </motion.div>
             )}
           </motion.div>
