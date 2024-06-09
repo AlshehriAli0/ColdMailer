ecd15011c83b7505b1d6fff528c6f956dbbaac30
Ali Alshehri <Ali0fawzish@outlook.com>
2024-06-09 21:33:50 +0300

    refactor: Improve layout of stats-skeleton component

--- src/components/suspense/stats-skeleton.tsx
+++ src/components/suspense/stats-skeleton.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function StatsSkeleton() {
   return (
     <section className="mb-8 grid grid-rows-2 gap-4 text-violet-300 md:w-[95%]">
-      <section className="flex gap-4">
+      <section className="flex h-fit gap-4">
         <div className="flex-1 rounded-xl border border-white/10">
-          <div className="p-6">
+          <div className="p-5">
             <h4 className="text-lg font-bold">Accepted Status</h4>
             <p className="text-violet-200/40">Total</p>
             <span className="flex items-center">
@@ -14,7 +14,7 @@ export default function StatsSkeleton() {
           </div>
         </div>
         <div className="flex-1 rounded-xl border border-white/10">
-          <div className="p-6">
+          <div className="p-5">
             <h4 className="text-lg font-bold">Rejected Status</h4>
             <p className="text-violet-200/40">Total</p>
             <span className="flex items-center">
@@ -23,7 +23,11 @@ export default function StatsSkeleton() {
           </div>
         </div>
       </section>
-      <div className="flex-1 rounded-xl border border-white/10"></div>
+      <div className="-mt-[4.4rem] h-[20rem] flex-1 rounded-xl border border-white/10 p-5 md:-mt-[5.3rem]">
+        <h4 className="text-lg font-bold">Sent Emails</h4>
+        <p className="text-violet-200/40">This Year</p>
+        <div className="flex h-[70%] mt-4 w-full animate-pulse gap-4 rounded bg-gray-300/10"></div>
+      </div>
     </section>
   );
 }
