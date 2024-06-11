86e69ee1134b42727888a8755b8f9d8a23436a5c
Ali Alshehri <Ali0fawzish@outlook.com>
2024-06-11 18:53:16 +0300

    ux improvements

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import GridButtons from "./grid-buttons";
 import RecipientList from "./recipient-list";
 import TrackerStats from "./tracker-stats";
+import { IoMdInformationCircleOutline } from "react-icons/io";
 
 export default async function Dashboard() {
   const { recipients, totals } = await fetchData();
@@ -10,6 +11,13 @@ export default async function Dashboard() {
   return (
     <main>
       <TrackerStats totals={totals} />
+      <p className="text-sm text-violet-400/30 flex items-center gap-2 md:hidden 4">
+      <span className="text-lg">
+
+        <IoMdInformationCircleOutline />
+      </span>
+        Edit details by clicking on the wanted recipient.
+      </p>
       <GridButtons />
       <RecipientList initialRecipients={recipients} />
     </main>
