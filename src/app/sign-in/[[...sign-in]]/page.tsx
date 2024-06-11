1755d31cf3b02daf60894eb9a4b33688a5423bb3
Ali Alshehri <Ali0fawzish@outlook.com>
2024-06-11 18:39:47 +0300

    refactor: updated rendering strategies for signup pages

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,8 @@
 import SignInForm from "@/components/sign-in";
 import React from "react";
 
+export const dynamic = "force-static";
+
 export default function SignInPage() {
   return (
     <section className="mt-[4rem] flex h-screen scroll-mt-96 justify-center sm:mt-[6rem] sm:w-full 2xl:mt-[9rem]">
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,12 +1,11 @@
-
-
 import SignUpForm from "@/components/sign-up";
 import React from "react";
 
+export const dynamic = "force-static";
 
 export default function SignUpPage() {
   return (
-    <section className="mt-[4rem] 2xl:mt-[9rem] flex h-screen w-full scroll-mt-96 justify-center sm:mt-[5rem]">
+    <section className="mt-[4rem] flex h-screen w-full scroll-mt-96 justify-center sm:mt-[5rem] 2xl:mt-[9rem]">
       <SignUpForm></SignUpForm>
     </section>
   );
